4338843d2b46b076fd2f5e56f667f705d14fbeeb
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-03-25 14:08:59 +0200

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $incomingRequest = $request->validate([
+            'name' => ['required'],
+            'email' => ['required'],
+            'password' => ['required'],
+            'role' => ['required'],
+        ]);
+
+        $incomingRequest['name'] = htmlspecialchars($incomingRequest['name']);
+        $incomingRequest['email'] = htmlspecialchars($incomingRequest['email']);
+        $incomingRequest['password'] = htmlspecialchars($incomingRequest['password']);
+
+        $user = Auth::create($incomingRequest);
+
+        if ($user) {
+        }
+    }
 }
---
+++ app/Services/MessageService.php
@@ -0,0 +1,14 @@
+
+<?php
+
+namespace App\Services;
+
+use App\Http\Controllers\Controller;
+
+class MessageService extends Controller
+{
+    public static function flash($type, $message)
+    {
+        session()->flash($type, $message);
+    }
+}
